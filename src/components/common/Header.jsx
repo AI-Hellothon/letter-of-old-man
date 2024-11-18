5ea58dc190c94efa030a27ac1a40995adbe9ba28
doyeon <dks729927@gmail.com>
2024-11-18 21:27:46 +0000

    feat: 헤더 구현

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,14 +1,31 @@
+import { ButtonContainer } from "./Header.style";
+
+import letterImage from "../../images/Header/letter.png";
+import settingImage from "../../images/Header/setting.png";
+
 const Header = () => {
   return (
     <div
       style={{
         padding: 15,
         display: "flex",
-        justifyContent: "space-between"
+        justifyContent: "space-between",
       }}
     >
-      <button>임시</button>
-      <button>임시</button>
+      <ButtonContainer>
+        <img
+          style={{ width: 53, height: 43 }}
+          src={letterImage}
+          alt="로고 이미지"
+        />
+      </ButtonContainer>
+      <ButtonContainer>
+        <img
+          style={{ width: 29, height: 32 }}
+          src={settingImage}
+          alt="설정 이미지"
+        />
+      </ButtonContainer>
     </div>
   );
 };
---
+++ src/components/common/Header.style.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+  border: none;
+  background: transparent;
+  padding: 0;
+
+  &:hover{
+    cursor: pointer;
+  }
+`;
